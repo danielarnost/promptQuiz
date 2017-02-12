7610c18b9710fe4b22d32bd0ef83c790978938af
danielarnost <danielarnost@utexaas.edu>
2017-02-12 08:40:23 -0600

    final

--- js/quiz.js
+++ js/quiz.js
@@ -5,17 +5,19 @@ var questions = [
 ];
 var correctAnswers = 0;
 var question;
-var answe
+var answer;
 var response;
 var correct = [];
 var wrong = [];
- 
+ // var html;
 
 function print(message) {
   var outputDiv = document.getElementById('output');
   outputDiv.innerHTML = message;
 }
 
+//this function creates a list of the answers to 'questions'
+//builds an ol after looping through questions and answers
 function buildList(arr){
 var listHTML = '<ol>';
 for (var i = 0; i < arr.length; i++){
@@ -25,13 +27,29 @@ listHTML += '<li>' + arr[i] + '</li>';
   return listHTML;
 }
 
-
-
-
-
-
-
-
-  for(var i=0; i < questions.length; i++) {
-  }
+//question postion 0 //answer postion 1
+//response variable is updated to be user input from prompt
+//answer is given in pisutio 1 above, then counters update
+for (var i = 0; i < questions.length; i++) {
+  question = questions[i][0];
+  answer = questions[i][1];
+  response = prompt(question);
+  response = parseInt(response);
+  if (response === answer) {
+    correctAnswers += 1;
+    correct.push(question);
+  }else{
+    wrong.push(question);
+  } 
 }
+
+html = "You got " + correctAnswers + " question(s) right."
+html += '<h2> You got these questions correct:</h2>';
+html += buildList(correct);
+html += '<h2> You got these questions wrong:</h2>';
+html += buildList(wrong);
+print(html);
+
+
+
+
